e18d348f0e7b35ca0269bc46ad2bb33f4650f81d
phhuong256 <1851061398@e.tlu.edu.vn>
2022-07-06 10:58:37 +0700

    test

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -90,6 +90,19 @@ router.get("/random", verify, async (req, res) => {
 
 //GET ALL
 
+// router.get("/", verify, async (req, res) => {
+//   if (req.user.isAdmin) {
+//     try {
+//       const movies = await Movie.find();
+//       res.status(200).json(movies.reverse());
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   } else {
+//     res.status(403).json("You are not allowed!");
+//   }
+// });
+
 router.get("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     try {
